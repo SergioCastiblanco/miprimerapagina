850f7d21d664915efee271006d5c685d77576b9e
xSACRx <65573631+SergioCastiblanco@users.noreply.github.com>
2023-11-02 19:53:56 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let menu_responsive = document.querySelector(".checkbtn");
+menu_responsive.onclick = function () {
+  navBar = document.querySelector(".navbar");
+  navBar.classList.toggle("active");
+};
+// alert("Hola este es mi JavaScript");
+let nombre = "Sergio";
+console.log(nombre);
+
+
+let acercaDe = document.querySelector("#tituloAcercaDeMi");
+// acercaDe.innerHTML = contenidoAcercaDeMi;
+
+let contenidoAcercaDeMi = acercaDe.innerText;
+// acercaDe.innerHTML = contenidoAcercaDeMi;
+console.log(contenidoAcercaDeMi);
+// acercaDe.innerHTML = acercaDe;
+
+// if ()
+
